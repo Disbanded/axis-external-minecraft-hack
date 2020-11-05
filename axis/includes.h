f57014f3e06f93c0044afbc4c011d250501b5082
Disbanded <60149515+Disbanded@users.noreply.github.com>
2020-11-05 09:29:58 +0000

    Add files via upload

---
+++ axis/includes.h
@@ -0,0 +1,11 @@
+#include <vector>
+
+std::vector<int> menuSize = { 500, 320 };
+int tab = 0;
+
+bool clickertoggled = false;
+bool aimassisttoggled = false;
+//bool combattoggled = false;
+//bool movementtoggled = false;
+//bool settingstoggled = false;
+bool velocitytoggled = false;
